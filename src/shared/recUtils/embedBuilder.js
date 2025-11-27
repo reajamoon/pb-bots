70953f7d8292c03d6a1bd2bd75b9ffaa08d9df82
reaja <reajamoon@pm.me>
2025-11-27 12:31:50 -0800

    Remove hardcoded credentials and deprecated embedBuilder.js

--- src/shared/recUtils/embedBuilder.js
+++
@@ -1,9 +0,0 @@
-// All the rec embed shenanigans live here. If something explodes, it's probably my fault.
-
-export async function createRecommendationEmbed(rec) {
-  // TODO: Move the actual code from rec.js. Yes, I know, still not done. Don't judge me.
-}
-
-export function createHelpPages() {
-  // Reminder: This chunk is still living in rec.js. One day I'll migrate it. Maybe.
-}
