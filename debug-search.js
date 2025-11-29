755f7d092d5bad367a323d688c14b35c1f94401d
reaja <reajamoon@pm.me>
2025-11-29 03:03:31 -0800

    enhance debug script to test embed creation

--- debug-search.js
+++ debug-search.js
@@ -1,5 +1,6 @@
 import { Recommendation } from './src/models/index.js';
 import { fetchRecWithSeries } from './src/models/fetchRecWithSeries.js';
+import { createRecEmbed } from './src/shared/recUtils/createRecEmbed.js';
 
 // Test a specific recommendation ID that should have series and tags
 const recId = process.argv[2] || 1; // Use first argument or default to 1
@@ -26,6 +27,7 @@ console.log('Has series?', !!recWithSeries.series);
 if (recWithSeries.series) {
     console.log('Series name:', recWithSeries.series.name);
     console.log('Series URL:', recWithSeries.series.url);
+    console.log('Series object keys:', Object.keys(recWithSeries.series.dataValues || recWithSeries.series));
 }
 
 console.log('Has userMetadata?', !!recWithSeries.userMetadata);
@@ -35,6 +37,23 @@ if (recWithSeries.userMetadata && recWithSeries.userMetadata.length > 0) {
         console.log('User metadata - additional_tags:', meta.additional_tags);
         console.log('User metadata - rec_note:', meta.rec_note);
     }
+}
+
+console.log('\n=== Testing embed creation ===');
+try {
+    const embed = createRecEmbed(recWithSeries);
+    const embedData = embed.toJSON();
+    console.log('Embed fields:');
+    embedData.fields?.forEach(field => {
+        console.log(`- ${field.name}: ${field.value}`);
+    });
+    
+    const hasSeriesField = embedData.fields?.some(field => field.name.includes('Series'));
+    const hasTagsField = embedData.fields?.some(field => field.name.includes('Tags'));
+    console.log('Has Series field?', hasSeriesField);
+    console.log('Has Tags field?', hasTagsField);
+} catch (err) {
+    console.error('Error creating embed:', err);
 }
 
 process.exit(0);
