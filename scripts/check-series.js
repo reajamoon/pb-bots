7c4964b014fb1636e4d18327372fd79f22f8b987
reaja <reajamoon@pm.me>
2025-11-29 02:31:25 -0800

    fix column names in series check script

--- scripts/check-series.js
+++ scripts/check-series.js
@@ -1,13 +1,13 @@
 import { Series } from '../src/models/index.js';
 
 const series = await Series.findAll({
-  attributes: ['id', 'ao3_id', 'title', 'works_count', 'createdAt'],
+  attributes: ['id', 'ao3SeriesId', 'name', 'workCount', 'createdAt'],
   order: [['createdAt', 'DESC']]
 });
 
 console.log('Current series entries:');
 series.forEach(s => {
-  console.log(`ID: ${s.id}, AO3 ID: ${s.ao3_id}, Title: ${s.title?.substring(0, 50)}..., Works: ${s.works_count}, Created: ${s.createdAt?.toISOString()?.split('T')[0]}`);
+  console.log(`ID: ${s.id}, AO3 ID: ${s.ao3SeriesId}, Name: ${s.name?.substring(0, 50)}..., Works: ${s.workCount}, Created: ${s.createdAt?.toISOString()?.split('T')[0]}`);
 });
 
 process.exit(0);
