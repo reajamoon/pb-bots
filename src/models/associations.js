f9e939bc3f6b23127065c09a7e804d6f2473cef8
reajamoon <reajamoon@pm.me>
2025-12-04 18:05:06 -0800

    deploy scripts

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,12 +1,3 @@
-import sequelize from '../sequelize.js';
-import DeanSprints from './DeanSprints.js';
-import createUser from './User.js';
-
-// Initialize User from factory pattern used in this repo
-const User = createUser(sequelize);
-
-// Set up associations for sprint achievement access via user
-DeanSprints.belongsTo(User, { foreignKey: 'userId', targetKey: 'discordId', as: 'user' });
-User.hasMany(DeanSprints, { foreignKey: 'userId', sourceKey: 'discordId', as: 'sprints' });
-
-export { User, DeanSprints };
+// Deprecated: associations are defined in src/models/index.js
+// This file is intentionally left blank to avoid duplicate model declarations.
+export {};
