a320981994ba786644613dec02c61456e3b4f24d
reajamoon <reajamoon@pm.me>
2025-12-07 16:48:11 -0800

    dean ping update

--- src/bots/dean/commands/ping.js
+++ src/bots/dean/commands/ping.js
@@ -1,11 +1,12 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { emoji, EMOJIS } from '../../../shared/emojiStore.js';
 
 const data = new SlashCommandBuilder()
   .setName('ping')
   .setDescription('Dean: simple health check');
 
 async function execute(interaction) {
-  await interaction.reply({ content: 'Dean is online and listening.' });
+  await interaction.reply({ content: `${emoji(EMOJIS.a_dean_morning)} Yeah yeah, I'm awake.` });
 }
 
 export { data };
